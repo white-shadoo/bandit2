3ce782bebb85b665a2d080ed85df8b2857ee0865
shadoo <muhammed76mahmoud.ahmed@gmail.com>
2025-08-26 14:25:16 +0300

    modify setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
-try:
-    import pbr
-except ImportError:
-    import subprocess
-    subprocess.check_call(["pip", "install", "pbr"])
-    import pbr
 
 from setuptools import setup
 
